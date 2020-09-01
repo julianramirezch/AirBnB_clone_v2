516ae9ab842f8b624db1309de8b50da3ae9bc2c3
julianramirezch <julianramirezch1@gmail.com>
2020-09-01 18:44:15 +0000

    [add]: script that starts a Flask web application display states sorted by name

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''  script that starts a Flask web application '''
+
+from flask import Flask, render_template
+from models import storage, State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(error):
+    ''' remove the current SQLAlchemy Session'''
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    ''' Display HTML page States '''
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states.sort())
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
