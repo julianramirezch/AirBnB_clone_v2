d16e137181bb08b88ae6169d873336eab253b5a5
julianramirezch <julianramirezch1@gmail.com>
2020-09-01 09:28:21 -0500

    [add]: Flask web application display: Hello HBNBHBNB, C <text>, python is fool by default and python <text>

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+''' script that starts a Flask web application '''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    ''' Route Hello HBNB! '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbtn():
+    ''' Route display HBNB '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    ''' Route C is <text> '''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    ''' Route Python is <text> '''
+    return 'Python {}'.format(text.replace('_', ' '))
