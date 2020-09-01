2398318c0951e109677088a7700d36acfc5af45e
julianramirezch <julianramirezch1@gmail.com>
2020-09-01 20:38:47 +0000

    [refactor]: teardown and html loop

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,17 +8,17 @@ from models import storage, State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_db(*args, **kwargs):
-    ''' remove the current SQLAlchemy Session'''
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     ''' Display HTML page States '''
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(*args, **kwargs):
+    ''' remove the current SQLAlchemy Session'''
+    storage.close()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
