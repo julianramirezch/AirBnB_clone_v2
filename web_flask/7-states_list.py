8b262821be83db32eb5d468e57c378004e9538ff
julianramirezch <julianramirezch1@gmail.com>
2020-09-01 13:57:43 -0500

    [refactor]: sorted names

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,7 +18,7 @@ def teardown_db(error):
 def states_list():
     ''' Display HTML page States '''
     states = storage.all(State).values()
-    return render_template('7-states_list.html', states=states.sort())
+    return render_template('7-states_list.html', states=states)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
