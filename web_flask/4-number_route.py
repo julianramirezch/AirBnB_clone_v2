3851d93b698e9c7207adeda30f32021ca70dd1d6
julianramirezch <julianramirezch1@gmail.com>
2020-09-01 09:55:56 -0500

    [add]: Flask web application display: Hello HBNBHBNB, C <text>, python is fool by default and python <text>, display n only if is integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+''' script that starts a Flask web application '''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    ''' Route Hello HBNB! '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbtn():
+    ''' Route display HBNB '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    ''' Route C is <text> '''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    ''' Route Python is <text> '''
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_a_number(n):
+    ''' Route number '''
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
